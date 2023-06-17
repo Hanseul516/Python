51d0ab45ddcb576631ff75b6aba5afdd5f6017eb
최한슬 <hanseulo@naver.com>
2023-06-17 13:51:29 +0900

    23-06-17

---
+++ Study/4_탈출문자.py
@@ -0,0 +1,17 @@
+# \n : 줄바꿈
+print("백문이 불여일견\n백견이 불여일타")
+
+# 저는 "최한슬"입니다.
+print("저는 \"최한슬\"입니다.")  # \" \' : 문장 내에서 따옴표
+
+# \\ : 문장 내에서 하나의 \로 바뀜
+print("https:\\\\duseul.tistory.com\\manage")
+
+# \r : 커서를 맨 앞으로 이동
+print("red Apple\rPine")  #커서를 맨앞으로 가서 pine이 실행되기 때문에 red가 사라지는 것처럼 보임
+
+# \b : 백스페이스(한 글자 삭제)
+print("redd\bApple")   
+
+# \t : 탭
+print("red\tApple")
